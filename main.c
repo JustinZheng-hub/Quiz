0425b2055d179367e50384adb96a216c7b10ea25
JustinZheng-hub <justin.zheng.2@stonybrook.edu>
2024-11-19 11:53:13 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,38 @@
 The function should use pointers for both strings and dynamically allocate memory to hold the concatenated 
 result. The function should be called concatenate(char *str1, char *str2) and return a pointer to the newly concatenated string
 */
+
+//Concatenates - link (things) together in a chain or series.
+
 #include <stdio.h>
+
+// Function to concatenate two strings and return pointer
+void concatenate(char* str1,  char* str2)
+{
+    // Navigate to the end of the first string
+    while (*str1) {
+        ++str1;
+    }
+
+    // Copy characters of the second string to the end of the first string
+    while (*str2) {
+        *str1++ = *str2++;
+    }
+
+    // Add the null-terminating character
+    *str1 = '\0';
+}
 
 int main()
 {
-    //write your code here.
+    char string1[50] = "Hello, and welcome to: ";
+    char string2[] = "ESE 124";
+
+    // Call function to concatenate strings
+    concatenate(string1, string2);
+
+    // Output the concatenated string
+    printf("Concatenated String: %s\n", string1);
 
     return 0;
-
 }
